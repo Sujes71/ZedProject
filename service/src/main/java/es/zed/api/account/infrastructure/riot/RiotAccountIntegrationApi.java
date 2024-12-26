e37916323f34c4a9399089d67ef81bc2e643fa01
Sujes71 <jesus17.pg@gmail.com>
2024-12-26 19:45:32 +0100

    test

--- service/src/main/java/es/zed/api/account/infrastructure/riot/RiotAccountIntegrationApi.java
+++ service/src/main/java/es/zed/api/account/infrastructure/riot/RiotAccountIntegrationApi.java
@@ -35,4 +35,5 @@ public class RiotAccountIntegrationApi extends RestHandler {
   public Mono<AccountDto> getAccountByPuuId(String puuid) {
     return doCall(riotAccountUrlMapper.mapUrlGetAccountByPuuId(puuid), HttpMethod.GET, null, AccountDto.class);
   }
+
 }
