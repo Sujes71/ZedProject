46cdfb58f07af56547c0ca5c9d3383eb9fca98d6
Sujes71 <jesus17.pg@gmail.com>
2024-12-27 16:45:02 +0100

    fix

--- service/src/main/java/es/zed/api/account/domain/ports/outbound/RiotApiAccountPort.java
+++ service/src/main/java/es/zed/api/account/domain/ports/outbound/RiotApiAccountPort.java
@@ -1,9 +1,10 @@
 package es.zed.api.account.domain.ports.outbound;
 
+import static es.zed.api.shared.domain.ports.outbound.OutboundPort.requestEvent;
+
 import es.zed.api.account.domain.model.AccountFilter;
 import es.zed.api.account.infrastructure.riot.dto.AccountDto;
 import es.zed.api.shared.domain.model.Message;
-import es.zed.api.shared.domain.ports.outbound.OutboundPort;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
@@ -14,14 +15,12 @@ public class RiotApiAccountPort {
   public static final String GET_ACCOUNT_BY_RIOT_ID_ADDRESS = "getAccountByRiotIdAddress";
   public static final String GET_ACCOUNT_ME_ADDRESS = "getAccountMeAddress";
 
-  private final OutboundPort outboundPort;
 
-  public RiotApiAccountPort(OutboundPort outboundPort) {
-    this.outboundPort = outboundPort;
+  private RiotApiAccountPort() {
   }
 
     public Mono<AccountDto> getAccountByPuuId(String puuid) {
-      return outboundPort.requestEvent(
+      return requestEvent(
           Message.<String>builder()
               .address(GET_ACCOUNT_BY_PUUID_ADDRESS)
               .body(puuid)
@@ -30,7 +29,7 @@ public class RiotApiAccountPort {
   }
 
   public Mono<AccountDto> getAccountByRiotId(AccountFilter filter) {
-    return outboundPort.requestEvent(
+    return requestEvent(
         Message.<AccountFilter>builder()
             .address(GET_ACCOUNT_BY_RIOT_ID_ADDRESS)
             .body(filter)
@@ -39,7 +38,7 @@ public class RiotApiAccountPort {
   }
 
   public Mono<AccountDto> getAccountMe() {
-    return outboundPort.requestEvent(
+    return requestEvent(
         Message.<Void>builder()
             .address(GET_ACCOUNT_ME_ADDRESS)
             .body(null)
